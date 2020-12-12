08c8769f07665b7338e2efbf2a6d07a1357a229f
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2020-12-12 16:42:45 +0900

    createアクションをコントローラに定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,13 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
   end
   def new
-    @Tweet.new
+    @tweet = Tweet.new
+  end
+  def create
+    Tweet.create(tweet_params)
+  end
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
   end
 end
