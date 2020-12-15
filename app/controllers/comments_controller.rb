475693994f0d976514005b42d050660b7a640c02
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2020-12-15 21:13:35 +0900

    コメントしたらツイートの詳細ページ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
   def create
     Comment.create(comment_params)
+    redirect_to "/tweet/#{comment.tweet.id}"
   end
-  
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
